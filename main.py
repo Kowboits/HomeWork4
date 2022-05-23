f59265b62efd98fd0269cf8368f406d41c2d683a
Evgeny Kapranov <e.s.kapranov@yandex.ru>
2022-05-23 22:28:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+# geo_logs_sorted = [visit for visit in geo_logs if list(visit.values())[0][1] == 'Россия']
+geo_logs_sorted = [visit for visit in geo_logs if 'Россия' in list(visit.values())[0]]
+
+print(geo_logs_sorted)
